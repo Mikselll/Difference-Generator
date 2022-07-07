b344677495029c7efc2ab8c81dfd29cca5b33917
Колосов Станислав <kolosov.stas01@mail.ru>
2022-07-07 20:55:08 +1000

    update Gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,6 +9,6 @@ program
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 program.parse();
