395bd3413129d5c74db509124eeb72391370860f
Колосов Станислав <kolosov.stas01@mail.ru>
2022-07-13 23:40:50 +1000

    update index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import { resolve, extname } from 'path';
 import { readFileSync } from 'fs';
-import _ from 'lodash';
 import parse from './parsers.js';
 import makeTree from './makeTree.js';
-import stylish from './stylish.js';
+import formatSelection from './formatters/index.js';
 
 const readFile = (file) => (readFileSync(resolve(file), 'utf-8'));
 
@@ -12,8 +11,8 @@ const genDiff = (file1, file2, format = 'stylish') => {
   const obj2 = parse(readFile(file2), extname(file2).slice(1));
 
   const tree = makeTree(obj1, obj2);
-  
-  return stylish(tree);
+
+  return formatSelection(tree, format);
 };
 
 export default genDiff;
