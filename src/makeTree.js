7b9761cb33b93729eef90eed6c9302b3f21a6c16
Колосов Станислав <kolosov.stas01@mail.ru>
2022-07-13 23:41:14 +1000

    update makeTree

--- src/makeTree.js
+++ src/makeTree.js
@@ -2,23 +2,25 @@ import _ from 'lodash';
 
 const makeTree = (obj1, obj2) => {
   const keys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
-  
+
   const result = keys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
     if (!_.has(obj1, key)) {
-      return { name: key, value: value2, type: 'added', };
+      return { name: key, value: value2, type: 'added' };
     }
     if (!_.has(obj2, key)) {
-      return { name: key, value: value1, type: 'removed', };
+      return { name: key, value: value1, type: 'removed' };
     }
     if (_.isObject(value1) && _.isObject(value2)) {
-      return { name: key, type: 'nested', children: makeTree(value1, value2), };
+      return { name: key, type: 'nested', children: makeTree(value1, value2) };
     }
     if (value1 !== value2) {
-      return { name: key, value1, value2, type: 'changed', };
+      return {
+        name: key, value1, value2, type: 'changed',
+      };
     }
-    return { name: key, value: value1, type: 'unchanged', };
+    return { name: key, value: value1, type: 'unchanged' };
   });
   return result;
 };
